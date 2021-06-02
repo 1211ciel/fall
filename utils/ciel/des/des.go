7d130bd7d2f266dba5342f3c5d5a699c1685291c
anyetanxi2017 <50581436+anyetanxi2017@users.noreply.github.com>
2021-06-03 01:44:50 +0800

    2

--- utils/ciel/des/des.go
+++ utils/ciel/des/des.go
@@ -1,1 +1,17 @@
 package des
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+func DecryptByDES(c *cli.Context) error {
+	//key := c.String("key")
+	//msg := c.String("msg")
+	//cipher, err := des.NewCipher([]byte(key))
+	//if err != nil {
+	//	return err
+	//}
+
+	//cipher.Decrypt()
+	return nil
+}
--- utils/ciel/html/codeToHtml.go
+++ utils/ciel/html/codeToHtml.go
@@ -1,1 +1,17 @@
-package main
+package html
+
+import (
+	"github.com/1211ciel/fall/utils/input"
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+	"strings"
+)
+
+func CodeToHtml(*cli.Context) error {
+	data := input.GetContent("请输入文本")
+	data = strings.ReplaceAll(data, "<", "&lt")
+	data = strings.ReplaceAll(data, ">", "&gt")
+	color.Green("--------主人,我完成啦--------")
+	color.Green(data)
+	return nil
+}
